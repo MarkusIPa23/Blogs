e74c304a6934456f7be67dc246026127d8336735
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-01-09 14:27:04 +0200

    KAutkas

---
+++ views/components/navbar.php
@@ -0,0 +1,8 @@
+<header>
+ <nav>
+  <ul>
+   <li><a href="/">blogi</a></li>
+   <li><a hef ="story.php">Par mums</a></li>
+  </ul>
+ </nav>
+</header>
--- views/index.view.php
+++ views/index.view.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="lv">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blogs</title>
-</head>
-<body>
+<?php require "components/header.php" ?>
+<?php require "components/navbar.php" ?>
  <h1>Blogs</h1>
 
 
@@ -14,6 +8,9 @@
 <button>Meklet</button>
 </form>
 
+<?php if (count($posts) == 0) { ?>
+<p>Ei nahui</p>
+<?php } ?>
 
 <ul>
 <?php foreach ($posts as $post){ ?>
